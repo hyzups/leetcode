fbf9e6cbc86c61aa3780b8b7e2281646e9270c8c
Yaohai Zheng <yaohaizh@126.com>
2016-04-09 23:35:12 +0800

    Update

--- src/IncreastingTripletSubsequence.cpp
+++ src/IncreastingTripletSubsequence.cpp
@@ -15,7 +15,7 @@ public:
 		int c1 = INT_MAX, c2 = INT_MAX;
 
 		for (int i = 0; i < nums.size(); i++) {
-			if (nums[i] < c1) {
+			if (nums[i] <= c1) {
 				c1 = nums[i];
 			}
 			else if (nums[i] <= c2) {
